d5053484ea5421cded05a04fcace86351aa1e588
Benedikt Elser <boun@gmx.de>
2018-07-03 00:08:54 +0200

    Refactor scan for devices at appropriate place

--- hrdevice/src/org/runnerup/hr/ContentproviderHRDevice.java
+++ hrdevice/src/org/runnerup/hr/ContentproviderHRDevice.java
@@ -6,14 +6,17 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Handler;
-import android.provider.BaseColumns;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContentproviderHRDevice implements HRProvider {
 
     private HRClient hrClient = null;
     private Handler hrClientHandler = null;
     public static final String NAME = "ContentproviderHRDevice";
+    private static final Uri GADGETBRIDGE_AUTHORITY = Uri.parse("content://com.gadgetbridge.heartrate.provider");
 
     private Context ctx;
 
@@ -39,26 +42,35 @@ public class ContentproviderHRDevice implements HRProvider {
         this.hrClientHandler = handler;
 
         Log.e(getClass().getName(), "open");
-        ContentResolver resolver = ctx.getContentResolver();
-        String[] projection = new String[]{BaseColumns._ID};
-        Uri provider = Uri.parse("content://com.gadgetbridge.heartrate.provider/devices");
-        Log.e(getClass().getName(), "Sending Query");
 
-        Cursor cursor = resolver.query(provider, projection, null, null, null);
-        if (cursor.moveToFirst()) {
+        getDevices();
+
+        //TODO: Subscribe to Contentprovider
+        hrClient.onOpenResult(true);
+    }
+
+    private List<HRDeviceRef> getDevices() {
+
+        List<HRDeviceRef> ret = new ArrayList<>();
+
+        ContentResolver resolver = ctx.getContentResolver();
+        Uri devices = GADGETBRIDGE_AUTHORITY.buildUpon().appendPath("devices").build();
+
+        Log.e(getClass().getName(), "Sending Query to " + devices.toString());
+
+        Cursor cursor = resolver.query(devices, null, null, null, null);
+        if (cursor != null && cursor.moveToFirst()) {
             do {
-                long id = cursor.getLong(0);
+                String deviceName = cursor.getString(0);
+                String deviceModel = cursor.getString(1);
+                String deviceAddress = cursor.getString(2);
+                ret.add(HRDeviceRef.create(getProviderName(), deviceName, deviceAddress));
                 // do something meaningful
             } while (cursor.moveToNext());
         } else {
             Log.e(getClass().getName(), "No Reults from Curosr");
         }
-
-
-
-
-                //TODO: Subscribe to Contentprovider
-        hrClient.onOpenResult(true);
+        return ret;
     }
 
     @Override
@@ -79,8 +91,9 @@ public class ContentproviderHRDevice implements HRProvider {
 
         Log.e(getClass().getName(), "StartScan");
 
-        //TODO ask Gadgetbridge for all devices
-        hrClient.onScanResult(HRDeviceRef.create(getProviderName(), "device-"+getProviderName(), "addr-"+NAME));
+        //TODO maybe move to thread
+        for (HRDeviceRef dev: getDevices())
+            hrClient.onScanResult(dev);
     }
 
     @Override
